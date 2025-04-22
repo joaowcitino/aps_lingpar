fd01f0c79285ad34bce6aac14c43e27acade06c7
Joao Citino <joaocitino10@gmail.com>
2025-04-21 22:22:43 -0300

    fix

--- docs/examples/hello_world.tf
+++ docs/examples/hello_world.tf
@@ -1,17 +1,21 @@
 boot HelloWorld
 
+  # Definir uma string
   chip message = "Hello, TechFlow World!"
-
+  
+  # Exibir a mensagem
   log message
   
-  module add(byte a, byte b)
-    process
-      return a + b
-    module_end
-
+  # Função de adição
+  module add(byte a, byte b) process
+    return a + b
+  module_end
+  
+  # Usar a função
   byte result = add(5, 3)
   log result
-
+  
+  # Estrutura condicional
   scan_begin
     ping result > 7 process
       log "Result is greater than 7"
@@ -21,13 +25,15 @@ boot HelloWorld
       log "Result is less than or equal to 7"
     pong_end
   scan_end
-
+  
+  # Loop while
   byte counter = 0
   stream counter < 5 process
     log counter
     counter = counter + 1
   stream_end
-
+  
+  # Loop for
   loop 5 as i process
     log i
   loop_end
